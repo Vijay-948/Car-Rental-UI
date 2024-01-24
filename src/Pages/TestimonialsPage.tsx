a0074c0eec9b306b43fb7f16b358d48a79a1d86d
Vijay-948 <kvijayreddy948@gmail.com>
2024-01-24 23:25:43 +0530

    testimonials

--- src/Pages/TestimonialsPage.tsx
+++ src/Pages/TestimonialsPage.tsx
@@ -1,7 +1,25 @@
+import Testimonials from "../Components/Testmonials";
 
 function TestimonialsPage() {
     return(
         <>
+        <section className="testmonials-page">
+            <Testimonials />
+
+            <div className="book-banner">
+                <div className="book-banner__overlay">
+                    <div className="container">
+                        <div className="text-content">
+                            <h2>Book a  car by getting in touch with us</h2>
+                            <span>
+                                <i className="fa-solid fa-phone"></i>
+                                <h3>+91 1234567890</h3>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         </>
     )
 
